db42e2dd05cecd5aaf1e004923a010a307da259b
handique2002 <72488436+handique2002@users.noreply.github.com>
2023-12-20 13:27:21 +0530

    Update check2.java

--- Sample/src/Drivers/check2.java
+++ Sample/src/Drivers/check2.java
@@ -5,7 +5,7 @@ public class check2 {
 	public static void main(String[] args) {
 		
 		
-		webdriver driver=new chromeDriver();
+		webdriver driver=new edgeDriver();
 		driver.get();
 		driver.findElement();
 		driver.findElements();
